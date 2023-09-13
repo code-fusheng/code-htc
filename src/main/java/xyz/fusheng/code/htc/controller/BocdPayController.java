ee86a0d17e4b4a684e958b019e7c7b9535444522
code-fusheng <2561035977@qq.com>
2023-09-13 20:01:07 +0800

    version : commit

---
+++ src/main/java/xyz/fusheng/code/htc/controller/BocdPayController.java
@@ -0,0 +1,15 @@
+package xyz.fusheng.code.htc.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author code-fusheng <2561035977@qq.com>
+ * @desc BocdPayController
+ * @date 2023-09-04 5:16 PM:12
+ */
+
+@RestController("/v1/api/bocdpay")
+public class BocdPayController {
+
+}
+
